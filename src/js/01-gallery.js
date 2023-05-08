9a59807df222c4d033c710ae7e2cf70ec500384e
soberjan1 <aliguseynov48@gmail.com>
2023-05-09 00:08:43 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+// РАМЗЕТКА
+const galleryItemsEl = document.querySelector('.gallery');
+const addGalleryItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+galleryItemsEl.insertAdjacentHTML('afterbegin', addGalleryItems);
+
+// БИБЛИОТЕКА SimpleLightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
