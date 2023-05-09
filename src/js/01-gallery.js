6d9515c87a2312a5b9a4cf84a531aef3989ca31a
soberjan1 <aliguseynov48@gmail.com>
2023-05-09 11:52:49 +0300

    ///

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
