5ac92b65eee6e6bc39c70fcf2eb3d2f8ae61cd31
soberjan1 <aliguseynov48@gmail.com>
2023-05-10 19:29:23 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player', {
-  id: 19231868,
-  width: 640,
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
